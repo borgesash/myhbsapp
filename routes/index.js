b8429f9e2465e190eeafff7b0f9b3d2576cf2734
Ashwin <ashwin@ashwin.com>
2016-02-09 14:31:10 -0500

    updated the app to use pg sql

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,20 @@ router.get('/', function(req, res) {
             );
 });
 
+var pg = require('pg');
+
+router.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send({results: result.rows} ); }
+    });
+  });
+});
+
 /* GET speakers page. */
 router.get('/speakers', function(req, res) {
   var myArtwork = [];
